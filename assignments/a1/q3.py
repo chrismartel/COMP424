da5a8da2891b507b54884fde018a87bece3f9302
Christian Martel <christian.martel2@mail.mcgill.ca>
2022-01-20 22:51:25 -0500

    a1 q3: implemented hill climbing

--- assignments/a1/q3.py
+++ assignments/a1/q3.py
@@ -15,10 +15,10 @@ def main():
         tsp_instances.append(TSP(number_of_cities))
 
     # a) Solve each TSP by brute-force
-    print('Brute-Force')
+    print('Solution')
     costs = list()
     for i in range(len(tsp_instances)):
-        tsp_instances[i].brute_force()
+        tsp_instances[i].optimal_state, tsp_instances[i].optimal_cost = tsp_instances[i].brute_force()
         costs.append(tsp_instances[i].optimal_cost)
 
     min_cost = min(costs)
@@ -50,7 +50,26 @@ def main():
 
 
     # c) Hill Climbing
+    print('Hill Climbing')
+    costs = list()
+    optimal_count = 0
+    for i in range(len(tsp_instances)):
+        final_state, final_cost = tsp_instances[i].hill_climbing()
+        if final_cost <= tsp_instances[i].optimal_cost:
+            optimal_count += 1
+        costs.append(final_cost)
+        
 
+    min_cost = min(costs)
+    max_cost = max(costs)
+    mean_cost = statistics.mean(costs)
+    std_cost = statistics.stdev(costs)
+
+    print("min: {min_cost:.6f}".format(min_cost=min_cost))
+    print("max: {max_cost:.6f}".format(max_cost=max_cost))
+    print("mean: {mean_cost:.6f}".format(mean_cost=mean_cost))
+    print("std: {std_cost:.6f}".format(std_cost=std_cost))
+    print("{optimal_count} instances found the optimal solution.\n".format(optimal_count=optimal_count))
     # d) Scale up!
 
 """
@@ -99,18 +118,20 @@ TSP problem instance
 """
 class TSP:
     def __init__(self, number_of_cities):
+        # initial config
         self.initial_state = list()
-        self.current_state = list()
+
+        # optimal config
         self.optimal_state = list()
         self.optimal_cost = sys.float_info.max
+
+        # generate random city coordinates
         for i in range(number_of_cities):
             self.initial_state.append((random.random(), random.random()))
-            self.current_state = self.initial_state
     
     """
     Solve by Brute Force
-    Generate all possible city permutations and find the 
-    tour with the minimum cost.
+    returns the following tuple (optimal_state, optimal_cost)
     """
     def brute_force(self):
         permutations = list(itertools.permutations(self.initial_state, len(self.initial_state)))
@@ -121,8 +142,38 @@ class TSP:
             if cost < min_cost:
                 min_cost = cost
                 min_index = i
-        self.optimal_state = permutations[min_index]
-        self.optimal_cost = min_cost
+        return (permutations[min_index], min_cost)
+
+    """
+    Solve by Hill Climbing
+    returns the following tuple (final_state, final_cost)
+    """
+    def hill_climbing(self):
+        # initialization
+        current_state = self.initial_state
+        current_cost = state_cost(self.initial_state)
+
+        # repeat until local minimum found
+        while(True):
+            min_cost = sys.float_info.max
+            min_index = 0
+            # generate set of neighbours of current state
+            neighbours = state_neighbours(current_state)
+
+            for i in range(len(neighbours)):
+                cost = state_cost(neighbours[i])
+                if cost < min_cost:
+                    min_cost = cost
+                    min_index = i
+
+            if min_cost >= current_cost:
+                # we are at a local min
+                return (current_state, current_cost)
+            else:
+                # take a greedy step towards local optimum
+                current_state = neighbours[min_index]
+                current_cost = min_cost
+
 
 if __name__ == "__main__":
     main()
